4a26d443b2858e422b27f3aa277df108203063ad
Quentin Robard <quentin.robard@etu.univ-lehavre.fr>
2019-12-24 14:43:54 +0100

    ajout du nom des cartes dans des tableaux

---
+++ js/initCards.js
@@ -0,0 +1,23 @@
+const neutralCardsBronze   = ["Clear_Skies_4.png", "Highwaymen_3x4.png", "Moon_Dust_5.png", "Peasant_Militia_2x4.png", "Rock_Barrage_6.png", "Spring_Equinox_4.png"]
+const monsterCardsBronze   = ["Alp_3x5.png", "Bruxa_2x4.png", "Endrega_Warrior_4x4.png", "Foglet_3x5.png", "Kikimore_Warrior_3x4.png", "Predatory_Dive_5.png"]
+const nilfgaardCardsBronze = ["Ard_Feainn_Light_Cavalry_2x4.png", "Assassination_6.png", "Ducal_Guard_3x4.png", "Recruit_4x4.png", "Slave_Hunter_1x5.png", "Tourney_Joust_4.png"]
+const scoiataelCardsBronze = ["Brokilon_Sentinel_2x5.png", "Dryad_Fledgling_3x4.png", "Dryad_Grovekeeper_3x4.png", "Forest_Whisperer_3x5.png", "Miner_3x5.png", "Tempering_4.png"]
+const skelligeCardsBronze  = ["Dimun_Corsair_3x5.png", "Dimun_Pirate_5x5.png", "Disgraced_Brawler_7x5.png", "Drummond_Warmonger_4x5.png", "Svalblod_Butcher_4x4.png", "Tuirseach_Axeman_2x4.png"]
+const nothernCardsBronze   = ["Cintrian_Artificer_3x5.png", "Cintrian_Enchantress_3x4.png", "Cintrian_Envoy_3x5.png", "Cintrian_Knight_2x4.png", "Cintrian_Royal_Guard_4x5.png", "Cintrian_Spellweaver_4x5.png"]
+
+const bronzeCards = [].concat( neutralCardsBronze, monsterCardsBronze, nilfgaardCardsBronze, scoiataelCardsBronze, skelligeCardsBronze, nothernCardsBronze )
+
+console.log( bronzeCards )
+
+const neutralCardsGold   = []
+const monsterCardsGold   = []
+const nilfgaardCardsGold = []
+const scoiataelCardsGold = []
+const skelligeCardsGold  = []
+const nothernCardsGold   = []
+
+const goldCards = [].concat( neutralCardsGold, monsterCardsGold, nilfgaardCardsGold, scoiataelCardsGold, skelligeCardsGold, nothernCardsGold )
+
+const allCards = [].concat( bronzeCards, goldCards )
+
+console.log( allCards )
